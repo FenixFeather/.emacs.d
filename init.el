3aa15747b0d31a87f7a293021f826417936fd353
FenixFeather <thomasliu02@gmail.com>
2019-06-14 15:34:47 -0700

    Update melpa to https

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; use-package setup
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
