deb30dbce2bdf4bcb619d2403a3bcf2add83e74d
FenixFeather <thomasliu02@gmail.com>
2019-06-17 14:46:39 -0700

    Move custom out of init.el

---
+++ custom.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
+ '(custom-safe-themes
+   (quote
+    ("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(doc-view-continuous t)
+ '(doc-view-dvipdf-program "dvipdfm")
+ '(doc-view-resolution 300)
+ '(dtrt-indent-mode t nil (dtrt-indent))
+ '(inferior-julia-program-name "julia")
+ '(longlines-wrap-follows-window-size t)
+ '(org-indent-mode-turns-off-org-adapt-indentation nil)
+ '(org-startup-indented t)
+ '(org-startup-truncated nil)
+ '(package-selected-packages
+   (quote
+    (counsel swiper ivy smart-mode-line-atom-one-dark-theme smart-mode-line spacemacs-theme neotree zenburn-theme yaml-mode web-mode vue-mode visual-regexp-steroids toml-mode smex rjsx-mode powershell paredit markdown-mode julia-mode json-mode haskell-mode flycheck dtrt-indent dockerfile-mode csv-mode company-bibtex company-auctex company-anaconda cdlatex ace-window)))
+ '(reftex-cite-prompt-optional-args (quote maybe))
+ '(show-paren-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+(put 'downcase-region 'disabled nil)
