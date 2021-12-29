9abdcb1abfa77f53ac3ee2e35b9d15fe5994e299
FenixFeather <thomasliu02@gmail.com>
2021-12-29 14:57:48 -0700

    Insert automatically in with-editor mode

--- init-evil.el
+++ init-evil.el
@@ -12,7 +12,9 @@
     (setq evil-undo-system 'undo-tree)
     (setq evil-emacs-state-cursor '("light slate blue" box))
     (setq evil-normal-state-cursor '("peach puff" box))
-    (personal-keys-setup))
+    (setq evil-insert-state-cursor '("peach puff" bar))
+    (personal-keys-setup)
+    (add-hook 'with-editor-mode-hook 'evil-insert-state))
 
 (use-package evil-commentary
     :ensure t
